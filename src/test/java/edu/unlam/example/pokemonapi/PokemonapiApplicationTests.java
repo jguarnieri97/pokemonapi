79ab221ddc3d17f5794265ee838d8a904d06f128
Julián Guarnieri <jguarnieri@alumno.unlam.edu.ar>
2025-04-10 12:12:14 -0300

    Proyecto base

---
+++ src/test/java/edu/unlam/example/pokemonapi/PokemonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.unlam.example.pokemonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
